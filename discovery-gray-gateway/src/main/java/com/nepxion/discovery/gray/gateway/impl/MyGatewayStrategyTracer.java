cf03260a05cadf932d155dd9a2f6ba44b3c8c2ca
Nepxion <1394997@qq.com>
2019-08-10 15:44:35 +0800

    修改全链路灰度调用链日志输出

--- discovery-gray-gateway/src/main/java/com/nepxion/discovery/gray/gateway/impl/MyGatewayStrategyTracer.java
+++ discovery-gray-gateway/src/main/java/com/nepxion/discovery/gray/gateway/impl/MyGatewayStrategyTracer.java
@@ -9,6 +9,10 @@ package com.nepxion.discovery.gray.gateway.impl;
  * @version 1.0
  */
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.apache.commons.collections4.MapUtils;
 import org.jboss.logging.MDC;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,9 +28,15 @@ public class MyGatewayStrategyTracer extends DefaultGatewayStrategyTracer {
     public void trace(ServerWebExchange exchange) {
         super.trace(exchange);
 
-        // 输出到日志
-        MDC.put("traceid", "traceid=" + strategyContextHolder.getHeader("traceid"));
-        MDC.put("spanid", "spanid=" + strategyContextHolder.getHeader("spanid"));
+        // 自定义调用链
+        Map<String, String> customerTraceMap = getCustomerTraceMap();
+        if (MapUtils.isNotEmpty(customerTraceMap)) {
+            for (Map.Entry<String, String> entry : customerTraceMap.entrySet()) {
+                MDC.put(entry.getKey(), entry.getKey() + "=" + entry.getValue());
+            }
+        }
+
+        // 灰度路由调用链
         MDC.put(DiscoveryConstant.N_D_SERVICE_GROUP, "服务组名=" + strategyContextHolder.getHeader(DiscoveryConstant.N_D_SERVICE_GROUP));
         MDC.put(DiscoveryConstant.N_D_SERVICE_TYPE, "服务类型=" + strategyContextHolder.getHeader(DiscoveryConstant.N_D_SERVICE_TYPE));
         MDC.put(DiscoveryConstant.N_D_SERVICE_ID, "服务名=" + strategyContextHolder.getHeader(DiscoveryConstant.N_D_SERVICE_ID));
@@ -45,5 +55,14 @@ public class MyGatewayStrategyTracer extends DefaultGatewayStrategyTracer {
         MDC.clear();
 
         LOG.info("全链路灰度调用链清除");
+    }
+
+    @Override
+    public Map<String, String> getCustomerTraceMap() {
+        Map<String, String> customerTraceMap = new LinkedHashMap<String, String>();
+        customerTraceMap.put("traceid", strategyContextHolder.getHeader("traceid"));
+        customerTraceMap.put("spanid", strategyContextHolder.getHeader("spanid"));
+
+        return customerTraceMap;
     }
 }
--- discovery-gray-service/src/main/java/com/nepxion/discovery/gray/service/impl/MyServiceStrategyTracer.java
+++ discovery-gray-service/src/main/java/com/nepxion/discovery/gray/service/impl/MyServiceStrategyTracer.java
@@ -9,7 +9,11 @@ package com.nepxion.discovery.gray.service.impl;
  * @version 1.0
  */
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import org.aopalliance.intercept.MethodInvocation;
+import org.apache.commons.collections4.MapUtils;
 import org.jboss.logging.MDC;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,9 +29,15 @@ public class MyServiceStrategyTracer extends DefaultServiceStrategyTracer {
     public void trace(ServiceStrategyTracerInterceptor interceptor, MethodInvocation invocation) {
         super.trace(interceptor, invocation);
 
-        // 输出到日志
-        MDC.put("traceid", "traceid=" + strategyContextHolder.getHeader("traceid"));
-        MDC.put("spanid", "spanid=" + strategyContextHolder.getHeader("spanid"));
+        // 自定义调用链
+        Map<String, String> customerTraceMap = getCustomerTraceMap();
+        if (MapUtils.isNotEmpty(customerTraceMap)) {
+            for (Map.Entry<String, String> entry : customerTraceMap.entrySet()) {
+                MDC.put(entry.getKey(), entry.getKey() + "=" + entry.getValue());
+            }
+        }
+
+        // 灰度路由调用链
         MDC.put(DiscoveryConstant.N_D_SERVICE_GROUP, "服务组名=" + pluginAdapter.getGroup());
         MDC.put(DiscoveryConstant.N_D_SERVICE_TYPE, "服务类型=" + pluginAdapter.getServiceType());
         MDC.put(DiscoveryConstant.N_D_SERVICE_ID, "服务名=" + pluginAdapter.getServiceId());
@@ -51,5 +61,14 @@ public class MyServiceStrategyTracer extends DefaultServiceStrategyTracer {
         MDC.clear();
 
         LOG.info("全链路灰度调用链清除");
+    }
+
+    @Override
+    public Map<String, String> getCustomerTraceMap() {
+        Map<String, String> customerTraceMap = new LinkedHashMap<String, String>();
+        customerTraceMap.put("traceid", strategyContextHolder.getHeader("traceid"));
+        customerTraceMap.put("spanid", strategyContextHolder.getHeader("spanid"));
+
+        return customerTraceMap;
     }
 }
