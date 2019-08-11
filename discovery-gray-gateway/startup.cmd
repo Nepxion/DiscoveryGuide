0cf87946d370af7168f4c8b353809eb11a28af5d
Nepxion <1394997@qq.com>
2019-08-11 18:12:44 +0800

    去掉startup.cmd

--- discovery-gray-gateway/startup.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-call java -jar discovery-gray-gateway-1.0.0.jar --nepxion.banner.shown.ansi.mode=true
--- discovery-gray-zuul/startup.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-call java -jar discovery-gray-zuul-1.0.0.jar --nepxion.banner.shown.ansi.mode=true
