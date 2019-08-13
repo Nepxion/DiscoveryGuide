ce5252bb35ceb7e7d4f468f62a81c127b16abc52
Nepxion <1394997@qq.com>
2019-08-13 12:52:44 +0800

    增加自动化测试模块

---
+++ discovery-gray-test/src/main/java/com/nepxion/discovery/gray/test/DiscoveryGrayTest.java
@@ -0,0 +1,29 @@
+package com.nepxion.discovery.gray.test;
+
+/**
+ * <p>Title: Nepxion Discovery</p>
+ * <p>Description: Nepxion Discovery</p>
+ * <p>Copyright: Copyright (c) 2017-2050</p>
+ * <p>Company: Nepxion</p>
+ * @author Haojun Ren
+ * @version 1.0
+ */
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = DiscoveryGrayTestApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class DiscoveryGrayTest {
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void test() throws Exception {
+        System.out.println("abc");
+    }
+}
