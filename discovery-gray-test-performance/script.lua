243cbee12c1055ca81193ada1f23084f690decc9
Nepxion <1394997@qq.com>
2019-08-23 21:09:02 +0800

    修改脚本

--- discovery-gray-test-performance/script.lua
+++ discovery-gray-test-performance/script.lua
@@ -1,17 +1,16 @@
-wrk.method = "POST"
-wrk.body   = ""
+wrk.method="POST"
+wrk.body=""
 wrk.headers["app"]="demo-service"
 wrk.headers["content-type"]="application/json"
 wrk.headers["token"]=""
 
-
 function request()
   return wrk.format("GET","/demo/ip",wrk.headers)
-  --return wrk.request()
+  -- return wrk.request()
 end
 
-function response(status, headers, body)
-if status ~= 200 then
-     print(body)
-   end
+function response(status,headers,body)
+  if status ~= 200 then
+    print(body)
+  end
 end
