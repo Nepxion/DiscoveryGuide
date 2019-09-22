df0a3ca31da628515c6767b2a004924c416eb54a
Nepxion <1394997@qq.com>
2019-09-23 05:35:20 +0800

    重构模块名

--- discovery-guide-test-performance/post.lua
+++ discovery-guide-test-performance/post.lua
@@ -2,4 +2,4 @@ wrk.method = "POST"
 wrk.body = '{"userId": "10001","coinType": "GT","type": "2","amount": "5.1"}'
 wrk.headers["id"] = "123"
 wrk.headers["token"] = "abc"
-wrk.headers["Content-Type"] = "application/json" 
+wrk.headers["Content-Type"] = "application/json"
