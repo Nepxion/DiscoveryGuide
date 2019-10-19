07f5620b6fefd803a7438187a1ea5077953f4df3
Nepxion <1394997@qq.com>
2019-10-19 23:52:08 +0800

    增加编译脚本

---
+++ install-maven.bat
@@ -0,0 +1,23 @@
+@echo on
+@echo =============================================================
+@echo $                                                           $
+@echo $                     Nepxion Discovery                     $
+@echo $                                                           $
+@echo $                                                           $
+@echo $                                                           $
+@echo $  Nepxion Studio All Right Reserved                        $
+@echo $  Copyright (C) 2017-2050                                  $
+@echo $                                                           $
+@echo =============================================================
+@echo.
+@echo off
+
+@title Nepxion Discovery Guide
+@color 0a
+
+call mvn clean install -DskipTests -pl discovery-guide-service -am -DMainClass=com.nepxion.discovery.guide.service.DiscoveryGuideServiceA1
+call mvn clean install -DskipTests -pl discovery-guide-gateway -am -DMainClass=com.nepxion.discovery.guide.gateway.DiscoveryGuideGateway
+call mvn clean install -DskipTests -pl discovery-guide-zuul    -am -DMainClass=com.nepxion.discovery.guide.zuul.DiscoveryGuideZuul
+call mvn clean install -DskipTests -pl discovery-guide-console -am -DMainClass=com.nepxion.discovery.guide.console.DiscoveryGuideConsole
+
+pause
