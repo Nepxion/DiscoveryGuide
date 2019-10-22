8ce1d34ea38bc39d3fada5867be81d6630daaa62
Nepxion <1394997@qq.com>
2019-10-22 09:44:30 +0800

    修改示例

--- discovery-guide-service/src/main/java/com/nepxion/discovery/guide/service/impl/MyServiceStrategyZipkinTracer.java
+++ discovery-guide-service/src/main/java/com/nepxion/discovery/guide/service/impl/MyServiceStrategyZipkinTracer.java
@@ -9,14 +9,10 @@ package com.nepxion.discovery.guide.service.impl;
  * @version 1.0
  */
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 import org.aopalliance.intercept.MethodInvocation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.nepxion.discovery.common.constant.DiscoveryConstant;
 import com.nepxion.discovery.plugin.strategy.service.tracer.DefaultServiceStrategyTracer;
 import com.nepxion.discovery.plugin.strategy.service.tracer.ServiceStrategyTracerInterceptor;
 
@@ -28,50 +24,15 @@ public class MyServiceStrategyZipkinTracer extends DefaultServiceStrategyTracer 
     public void trace(ServiceStrategyTracerInterceptor interceptor, MethodInvocation invocation) {
         super.trace(interceptor, invocation);
 
-        // 自定义调用链
-        /*MDC.put("traceid", "traceid=" + strategyContextHolder.getHeader("traceid"));
-        MDC.put("spanid", "spanid=" + strategyContextHolder.getHeader("spanid"));
-        MDC.put("mobile", "mobile=" + strategyContextHolder.getHeader("mobile"));
-        MDC.put("user", "user=" + strategyContextHolder.getHeader("user"));
-
-        // 灰度路由调用链
-        MDC.put(DiscoveryConstant.N_D_SERVICE_GROUP, "服务组名=" + pluginAdapter.getGroup());
-        MDC.put(DiscoveryConstant.N_D_SERVICE_TYPE, "服务类型=" + pluginAdapter.getServiceType());
-        MDC.put(DiscoveryConstant.N_D_SERVICE_ID, "服务名=" + pluginAdapter.getServiceId());
-        MDC.put(DiscoveryConstant.N_D_SERVICE_ADDRESS, "地址=" + pluginAdapter.getHost() + ":" + pluginAdapter.getPort());
-        MDC.put(DiscoveryConstant.N_D_SERVICE_VERSION, "版本=" + pluginAdapter.getVersion());
-        MDC.put(DiscoveryConstant.N_D_SERVICE_REGION, "区域=" + pluginAdapter.getRegion());*/
+        // 待实现
 
         LOG.info("全链路灰度调用链输出到Zipkin");
-
-        String methodName = interceptor.getMethodName(invocation);
-        LOG.info("methodName={}", methodName);
-
-        Object[] arguments = interceptor.getArguments(invocation);
-        for (int i = 0; i < arguments.length; i++) {
-            LOG.info("arguments[{}]={}", i, arguments[i].toString());
-        }
     }
 
     @Override
     public void error(ServiceStrategyTracerInterceptor interceptor, MethodInvocation invocation, Throwable e) {
+        // 待实现
 
-    }
-
-    @Override
-    public void release(ServiceStrategyTracerInterceptor interceptor, MethodInvocation invocation) {
-
-    }
-
-    // Debug用
-    @Override
-    public Map<String, String> getDebugTraceMap() {
-        Map<String, String> debugTraceMap = new LinkedHashMap<String, String>();
-        debugTraceMap.put("traceid", strategyContextHolder.getHeader("traceid"));
-        debugTraceMap.put("spanid", strategyContextHolder.getHeader("spanid"));
-        debugTraceMap.put("mobile", strategyContextHolder.getHeader("mobile"));
-        debugTraceMap.put("user", strategyContextHolder.getHeader("user"));
-
-        return debugTraceMap;
+        LOG.info("全链路灰度调用链异常输出到Zipkin");
     }
 }
