dcaace1c51759ebefafda240f54b8d5c712a2362
Nepxion <1394997@qq.com>
2019-10-22 15:25:46 +0800

    重构类结构

--- discovery-guide-service/src/main/java/com/nepxion/discovery/guide/service/impl/MyServiceStrategyZipkinTracer.java
+++ discovery-guide-service/src/main/java/com/nepxion/discovery/guide/service/impl/MyServiceStrategyZipkinTracer.java
@@ -9,21 +9,22 @@ package com.nepxion.discovery.guide.service.impl;
  * @version 1.0
  */
 
-import com.nepxion.discovery.common.constant.DiscoveryConstant;
 import io.opentracing.Span;
 import io.opentracing.Tracer;
 import io.opentracing.tag.Tags;
-import org.aopalliance.intercept.MethodInvocation;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.nepxion.discovery.plugin.strategy.service.tracer.DefaultServiceStrategyTracer;
-import com.nepxion.discovery.plugin.strategy.service.tracer.ServiceStrategyTracerInterceptor;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+
+import org.aopalliance.intercept.MethodInvocation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.nepxion.discovery.common.constant.DiscoveryConstant;
+import com.nepxion.discovery.plugin.strategy.service.tracer.DefaultServiceStrategyTracer;
+import com.nepxion.discovery.plugin.strategy.service.tracer.ServiceStrategyTracerInterceptor;
 
 // 自定义调用链和灰度调用链输出到Zipkin
 public class MyServiceStrategyZipkinTracer extends DefaultServiceStrategyTracer {
@@ -31,8 +32,6 @@ public class MyServiceStrategyZipkinTracer extends DefaultServiceStrategyTracer 
 
     @Autowired
     private Tracer tracer;
-
-    private Tracer.SpanBuilder spanBuilder;
 
     private Span span;
 
@@ -43,34 +42,42 @@ public class MyServiceStrategyZipkinTracer extends DefaultServiceStrategyTracer 
         Method method = invocation.getMethod();
         String methodName = interceptor.getMethodName(invocation);
         String className = method.getDeclaringClass().getName();
+
         Tracer.SpanBuilder spanBuilder = tracer.buildSpan(methodName);
-        spanBuilder.withTag(Tags.COMPONENT.getKey(),"discovery");
-        spanBuilder.withTag("methodName",methodName);
-        spanBuilder.withTag("className",className);
-        Span span = spanBuilder.start();
+        spanBuilder.withTag(Tags.COMPONENT.getKey(), "discovery");
+        spanBuilder.withTag("methodName", methodName);
+        spanBuilder.withTag("className", className);
+
+        span = spanBuilder.start();
+
+        // 灰度路由调用链
         span.setTag(DiscoveryConstant.N_D_SERVICE_GROUP, "服务组名=" + pluginAdapter.getGroup());
         span.setTag(DiscoveryConstant.N_D_SERVICE_TYPE, "服务类型=" + pluginAdapter.getServiceType());
         span.setTag(DiscoveryConstant.N_D_SERVICE_ID, "服务名=" + pluginAdapter.getServiceId());
         span.setTag(DiscoveryConstant.N_D_SERVICE_ADDRESS, "地址=" + pluginAdapter.getHost() + ":" + pluginAdapter.getPort());
         span.setTag(DiscoveryConstant.N_D_SERVICE_VERSION, "版本=" + pluginAdapter.getVersion());
         span.setTag(DiscoveryConstant.N_D_SERVICE_REGION, "区域=" + pluginAdapter.getRegion());
+
         LOG.info("全链路灰度调用链输出到Zipkin");
     }
 
     @Override
     public void error(ServiceStrategyTracerInterceptor interceptor, MethodInvocation invocation, Throwable e) {
-        Map<String,Object> exceptionMap = new HashMap<>();
-        exceptionMap.put("event",Tags.ERROR.getKey());
-        exceptionMap.put("error.object",e);
+        Map<String, Object> exceptionMap = new HashMap<>();
+        exceptionMap.put("event", Tags.ERROR.getKey());
+        exceptionMap.put("error.object", e);
+
         span.log(exceptionMap);
+
         LOG.info("全链路灰度调用链异常输出到Zipkin");
     }
 
     @Override
     public void release(ServiceStrategyTracerInterceptor interceptor, MethodInvocation invocation) {
-        super.release(interceptor, invocation);
-        if (span != null && tracer != null){
+        if (span != null && tracer != null) {
             span.finish();
         }
+
+        LOG.info("全链路灰度调用链Zipkin上下文清除");
     }
 }
--- discovery-guide-zuul/src/main/java/com/nepxion/discovery/guide/zuul/impl/MyZuulStrategyZipkinTracer.java
+++ discovery-guide-zuul/src/main/java/com/nepxion/discovery/guide/zuul/impl/MyZuulStrategyZipkinTracer.java
@@ -9,18 +9,17 @@ package com.nepxion.discovery.guide.zuul.impl;
  * @version 1.0
  */
 
-import com.nepxion.discovery.common.constant.DiscoveryConstant;
-import com.nepxion.discovery.plugin.strategy.zuul.tracer.DefaultZuulStrategyTracer;
-import com.netflix.zuul.context.RequestContext;
 import io.opentracing.Span;
 import io.opentracing.Tracer;
 import io.opentracing.tag.Tags;
-import org.jboss.logging.MDC;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import java.util.LinkedHashMap;
-import java.util.Map;
+
+import com.nepxion.discovery.common.constant.DiscoveryConstant;
+import com.nepxion.discovery.plugin.strategy.zuul.tracer.DefaultZuulStrategyTracer;
+import com.netflix.zuul.context.RequestContext;
 
 // 自定义调用链和灰度调用链输出到Zipkin
 public class MyZuulStrategyZipkinTracer extends DefaultZuulStrategyTracer {
@@ -29,62 +28,33 @@ public class MyZuulStrategyZipkinTracer extends DefaultZuulStrategyTracer {
     @Autowired
     private Tracer tracer;
 
+    private Span span;
 
     @Override
     public void trace(RequestContext context) {
         super.trace(context);
 
-
         Tracer.SpanBuilder spanBuilder = tracer.buildSpan("opentracingSpan");
-        spanBuilder.withTag(Tags.COMPONENT.getKey(),"discovery");
-        Span span = spanBuilder.start();
-        //添加链路信息
+        spanBuilder.withTag(Tags.COMPONENT.getKey(), "discovery");
+        span = spanBuilder.start();
+
+        // 灰度路由调用链
         span.setTag(DiscoveryConstant.N_D_SERVICE_GROUP, "服务组名=" + strategyContextHolder.getHeader(DiscoveryConstant.N_D_SERVICE_GROUP));
         span.setTag(DiscoveryConstant.N_D_SERVICE_TYPE, "服务类型=" + strategyContextHolder.getHeader(DiscoveryConstant.N_D_SERVICE_TYPE));
         span.setTag(DiscoveryConstant.N_D_SERVICE_ID, "服务名=" + strategyContextHolder.getHeader(DiscoveryConstant.N_D_SERVICE_ID));
         span.setTag(DiscoveryConstant.N_D_SERVICE_ADDRESS, "地址=" + strategyContextHolder.getHeader(DiscoveryConstant.N_D_SERVICE_ADDRESS));
         span.setTag(DiscoveryConstant.N_D_SERVICE_VERSION, "版本=" + strategyContextHolder.getHeader(DiscoveryConstant.N_D_SERVICE_VERSION));
         span.setTag(DiscoveryConstant.N_D_SERVICE_REGION, "区域=" + strategyContextHolder.getHeader(DiscoveryConstant.N_D_SERVICE_REGION));
-        //TODO 这里最好能够捕捉方法调用异常，否则链路中异常无法捕获
-        if (span != null && tracer != null){
-            span.finish();
-        }
 
-        // 自定义调用链
-        MDC.put("traceid", "traceid=" + strategyContextHolder.getHeader("traceid"));
-        MDC.put("spanid", "spanid=" + strategyContextHolder.getHeader("spanid"));
-        MDC.put("mobile", "mobile=" + strategyContextHolder.getHeader("mobile"));
-        MDC.put("user", "user=" + strategyContextHolder.getHeader("user"));
-
-        // 灰度路由调用链
-        MDC.put(DiscoveryConstant.N_D_SERVICE_GROUP, "服务组名=" + strategyContextHolder.getHeader(DiscoveryConstant.N_D_SERVICE_GROUP));
-        MDC.put(DiscoveryConstant.N_D_SERVICE_TYPE, "服务类型=" + strategyContextHolder.getHeader(DiscoveryConstant.N_D_SERVICE_TYPE));
-        MDC.put(DiscoveryConstant.N_D_SERVICE_ID, "服务名=" + strategyContextHolder.getHeader(DiscoveryConstant.N_D_SERVICE_ID));
-        MDC.put(DiscoveryConstant.N_D_SERVICE_ADDRESS, "地址=" + strategyContextHolder.getHeader(DiscoveryConstant.N_D_SERVICE_ADDRESS));
-        MDC.put(DiscoveryConstant.N_D_SERVICE_VERSION, "版本=" + strategyContextHolder.getHeader(DiscoveryConstant.N_D_SERVICE_VERSION));
-        MDC.put(DiscoveryConstant.N_D_SERVICE_REGION, "区域=" + strategyContextHolder.getHeader(DiscoveryConstant.N_D_SERVICE_REGION));
-
-        LOG.info("全链路灰度调用链输出");
-
-        LOG.info("request={}", context.getRequest());
+        LOG.info("全链路灰度调用链输出到Zipkin");
     }
 
     @Override
     public void release(RequestContext context) {
-        MDC.clear();
+        if (span != null && tracer != null) {
+            span.finish();
+        }
 
-        LOG.info("全链路灰度调用链清除");
-    }
-
-    // Debug用
-    @Override
-    public Map<String, String> getDebugTraceMap() {
-        Map<String, String> debugTraceMap = new LinkedHashMap<String, String>();
-        debugTraceMap.put("traceid", strategyContextHolder.getHeader("traceid"));
-        debugTraceMap.put("spanid", strategyContextHolder.getHeader("spanid"));
-        debugTraceMap.put("mobile", strategyContextHolder.getHeader("mobile"));
-        debugTraceMap.put("user", strategyContextHolder.getHeader("user"));
-
-        return debugTraceMap;
+        LOG.info("全链路灰度调用链Zipkin上下文清除");
     }
 }
