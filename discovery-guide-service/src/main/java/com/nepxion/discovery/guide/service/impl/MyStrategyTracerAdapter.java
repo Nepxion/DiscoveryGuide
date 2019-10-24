90dc725b7158caa3432bc5379c62a40b80097b7e
Nepxion <1394997@qq.com>
2019-10-25 07:00:03 +0800

    修改示例

--- discovery-guide-service/src/main/java/com/nepxion/discovery/guide/service/impl/MyStrategyTracerAdapter.java
+++ discovery-guide-service/src/main/java/com/nepxion/discovery/guide/service/impl/MyStrategyTracerAdapter.java
@@ -17,6 +17,9 @@ import com.google.common.collect.ImmutableMap;
 import com.nepxion.discovery.common.constant.DiscoveryConstant;
 import com.nepxion.discovery.plugin.strategy.adapter.DefaultStrategyTracerAdapter;
 
+// 自定义调用链上下文参数的创建
+// 对于getTraceId和getSpanId方法，在Opentracing等调用链中间件引入的情况下，由调用链中间件决定，在这里定义不会起作用；在Opentracing等调用链中间件未引入的情况下，在这里定义才有效，下面代码中表示从Http Header中获取，并全链路传递
+// 对于getCustomizationMap方法，表示输出到调用链中的定制化业务参数，可以同时输出到日志和Opentracing等调用链中间件，下面代码中表示从Http Header中获取，并全链路传递
 public class MyStrategyTracerAdapter extends DefaultStrategyTracerAdapter {
     @Override
     public String getTraceId() {
