cc156ae59613420f2abe8187de82a59372987d30
HaojunRen <1394997@qq.com>
2020-08-14 16:52:22 +0800

    修改示例

--- discovery-guide-service/src/main/java/com/nepxion/discovery/guide/service/rest/ARestImpl.java
+++ discovery-guide-service/src/main/java/com/nepxion/discovery/guide/service/rest/ARestImpl.java
@@ -13,9 +13,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -30,7 +29,7 @@ public class ARestImpl extends CoreImpl {
     @Autowired
     private RestTemplate restTemplate;
 
-    @RequestMapping(path = "/rest/{value}", method = RequestMethod.GET)
+    @GetMapping(path = "/rest/{value}")
     public String rest(@PathVariable(value = "value") String value) {
         value = getPluginInfo(value);
         value = restTemplate.getForEntity("http://discovery-guide-service-b/rest/" + value, String.class).getBody();
