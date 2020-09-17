ca07ca12d2796eed69dc2e6271bd084c82982c1c
HaojunRen <1394997@qq.com>
2020-09-17 11:57:10 +0800

    增加异步示例

---
+++ discovery-guide-service/src/main/java/com/nepxion/discovery/guide/service/DiscoveryGuideServiceAsyncA1.java
@@ -0,0 +1,36 @@
+package com.nepxion.discovery.guide.service;
+
+/**
+ * <p>Title: Nepxion Discovery</p>
+ * <p>Description: Nepxion Discovery</p>
+ * <p>Copyright: Copyright (c) 2017-2050</p>
+ * <p>Company: Nepxion</p>
+ * @author Haojun Ren
+ * @version 1.0
+ */
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableAsync
+public class DiscoveryGuideServiceAsyncA1 {
+    public static void main(String[] args) {
+        System.setProperty("spring.profiles.active", "a1");
+
+        // 彩色旗标显示设置
+        System.setProperty("nepxion.banner.shown.ansi.mode", "true");
+
+        // 阿里巴巴Sentinel Dashboard设置
+        System.setProperty("project.name", "guide-service-a1");
+        System.setProperty("csp.sentinel.dashboard.server", System.getProperty("middleware.host", "localhost") + ":8075");
+        System.setProperty("csp.sentinel.api.port", "3002");
+
+        new SpringApplicationBuilder(DiscoveryGuideServiceAsyncA1.class).run(args);
+    }
+}
---
+++ discovery-guide-service/src/main/java/com/nepxion/discovery/guide/service/feign/AsyncAFeignImpl.java
@@ -0,0 +1,69 @@
+package com.nepxion.discovery.guide.service.feign;
+
+import io.opentracing.contrib.concurrent.TracedRunnable;
+import io.opentracing.util.GlobalTracer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.nepxion.discovery.common.constant.DiscoveryConstant;
+import com.nepxion.discovery.guide.service.core.CoreImpl;
+
+@RestController
+// @Permission(name = "AFeign", label = "AFeign label", description = "AFeign description")
+@ConditionalOnProperty(name = DiscoveryConstant.SPRING_APPLICATION_NAME, havingValue = "discovery-guide-service-a")
+public class AsyncAFeignImpl extends CoreImpl implements AsyncAFeign {
+    private static final Logger LOG = LoggerFactory.getLogger(AsyncAFeignImpl.class);
+
+    @Autowired
+    private BFeign bFeign;
+
+    @Override
+    @SentinelResource(value = "sentinel-resource", blockHandler = "handleBlock", fallback = "handleFallback")
+    @Async
+    public String invokeAsync(@PathVariable(value = "value") String value) {
+        /*value = invoke(value);
+
+        AsyncResult<String> result = new AsyncResult<String>(value);
+        try {
+            value = result.get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }*/
+
+        Runnable invokeRunnable = new Runnable() {
+            @Override
+            public void run() {
+                invoke(value);
+            }
+        };
+        TracedRunnable tracedRunnable = new TracedRunnable(invokeRunnable, GlobalTracer.get());
+        new Thread(tracedRunnable).start();
+
+        return "Invoke Async";
+    }
+    
+    private String invoke(String value) {
+        value = getPluginInfo(value);
+        value = bFeign.invoke(value);
+
+        LOG.info("调用路径：{}", value);
+        
+        return value;
+    }
+
+    public String handleBlock(String value, BlockException e) {
+        return value + "-> A server sentinel block, cause=" + e.getClass().getName() + ", rule=" + e.getRule() + ", limitApp=" + e.getRuleLimitApp();
+    }
+
+    public String handleFallback(String value) {
+        return value + "-> A server sentinel fallback";
+    }
+}
