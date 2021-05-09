bcf861074758c2448013abe6efb86616e570de59
HaojunRen <1394997@qq.com>
2021-05-09 11:49:27 +0800

    修改示例

--- discovery-guide-test-automation/src/test/java/com/nepxion/discovery/guide/test/DiscoveryGuideTest.java
+++ discovery-guide-test-automation/src/test/java/com/nepxion/discovery/guide/test/DiscoveryGuideTest.java
@@ -391,8 +391,8 @@ public class DiscoveryGuideTest {
 
     @Test
     public void testDynamicRoute() throws Exception {
-        discoveryGuideTestCases.testDynamicRoute1("nepxion", gatewayServiceId, new String[] {gatewayTestRoute0Url, gatewayTestRoute1Url, gatewayTestRoute2Url}, "dynamic-route-gateway-default.json");
-        discoveryGuideTestCases.testDynamicRoute2("nepxion", gatewayServiceId, new String[] {gatewayTestRoute0Url, gatewayTestRoute1Url, gatewayTestRoute2Url}, "dynamic-route-gateway.json", "dynamic-route-gateway-default.json");
+        discoveryGuideTestCases.testDynamicRoute1(gatewayGroup, gatewayServiceId + "-dynamic-route", new String[] {gatewayTestRoute0Url, gatewayTestRoute1Url, gatewayTestRoute2Url}, "dynamic-route-gateway-default.json");
+        discoveryGuideTestCases.testDynamicRoute2(gatewayGroup, gatewayServiceId + "-dynamic-route", new String[] {gatewayTestRoute0Url, gatewayTestRoute1Url, gatewayTestRoute2Url}, "dynamic-route-gateway.json", "dynamic-route-gateway-default.json");
     }
 
     /*@Test
