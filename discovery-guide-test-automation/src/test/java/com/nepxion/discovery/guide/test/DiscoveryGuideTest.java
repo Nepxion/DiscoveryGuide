ccdd53423aaaf53134593e068f599fffa70eeb5c
HaojunRen <1394997@qq.com>
2022-10-24 10:04:12 +0800

    修改配置

--- discovery-guide-test-automation/src/test/java/com/nepxion/discovery/guide/test/DiscoveryGuideTest.java
+++ discovery-guide-test-automation/src/test/java/com/nepxion/discovery/guide/test/DiscoveryGuideTest.java
@@ -55,8 +55,8 @@ public class DiscoveryGuideTest {
     @Value("${gray.weight.testcases.enabled:true}")
     private Boolean weightTestcasesEnabled;
 
-    @Value("${testcase.loop.times:1}")
-    private Integer loopTimes;
+    @Value("${testcase.loop.count:1}")
+    private Integer loopCount;
 
     @Autowired
     private DiscoveryGuideTestCases discoveryGuideTestCases;
@@ -80,7 +80,7 @@ public class DiscoveryGuideTest {
 
     @Test
     public void test1NoGray() throws Exception {
-        for (int i = 0; i < loopTimes; i++) {
+        for (int i = 0; i < loopCount; i++) {
             discoveryGuideTestCases.testNoGray(gatewayGroup, gatewayServiceId, gatewayTestUrl);
             discoveryGuideTestCases.testNoGray(gatewayGroup, gatewayGroup, gatewayTestUrl);
         }
@@ -98,77 +98,77 @@ public class DiscoveryGuideTest {
 
     @Test
     public void testEnabledStrategyGray1() throws Exception {
-        for (int i = 0; i < loopTimes; i++) {
+        for (int i = 0; i < loopCount; i++) {
             discoveryGuideTestCases.testEnabledStrategyGray1(gatewayTestUrl);
         }
     }
 
     @Test
     public void testEnabledStrategyGray2() throws Exception {
-        for (int i = 0; i < loopTimes; i++) {
+        for (int i = 0; i < loopCount; i++) {
             discoveryGuideTestCases.testEnabledStrategyGray2(gatewayTestUrl);
         }
     }
 
     @Test
     public void testVersionRouteFilter1() throws Exception {
-        for (int i = 0; i < loopTimes; i++) {
+        for (int i = 0; i < loopCount; i++) {
             discoveryGuideTestCases.testVersionRouteFilter1(gatewayTestUrl);
         }
     }
 
     @Test
     public void testVersionRouteFilter2() throws Exception {
-        for (int i = 0; i < loopTimes; i++) {
+        for (int i = 0; i < loopCount; i++) {
             discoveryGuideTestCases.testVersionRouteFilter2(gatewayTestUrl);
         }
     }
 
     @Test
     public void testRegionRouteFilter1() throws Exception {
-        for (int i = 0; i < loopTimes; i++) {
+        for (int i = 0; i < loopCount; i++) {
             discoveryGuideTestCases.testRegionRouteFilter1(gatewayTestUrl);
         }
     }
 
     @Test
     public void testRegionRouteFilter2() throws Exception {
-        for (int i = 0; i < loopTimes; i++) {
+        for (int i = 0; i < loopCount; i++) {
             discoveryGuideTestCases.testRegionRouteFilter2(gatewayTestUrl);
         }
     }
 
     @Test
     public void testAddressRouteFilter1() throws Exception {
-        for (int i = 0; i < loopTimes; i++) {
+        for (int i = 0; i < loopCount; i++) {
             discoveryGuideTestCases.testAddressRouteFilter1(gatewayTestUrl);
         }
     }
 
     @Test
     public void testAddressRouteFilter2() throws Exception {
-        for (int i = 0; i < loopTimes; i++) {
+        for (int i = 0; i < loopCount; i++) {
             discoveryGuideTestCases.testAddressRouteFilter2(gatewayTestUrl);
         }
     }
 
     @Test
     public void testVersionStrategyGray1() throws Exception {
-        for (int i = 0; i < loopTimes; i++) {
+        for (int i = 0; i < loopCount; i++) {
             discoveryGuideTestCases.testVersionStrategyGray1(gatewayGroup, gatewayServiceId, gatewayTestUrl);
         }
     }
 
     @Test
     public void testVersionStrategyGray2() throws Exception {
-        for (int i = 0; i < loopTimes; i++) {
+        for (int i = 0; i < loopCount; i++) {
             discoveryGuideTestCases.testVersionStrategyGray2(gatewayGroup, gatewayServiceId, gatewayTestUrl);
         }
     }
 
     @Test
     public void testVersionStrategyGray3() throws Exception {
-        for (int i = 0; i < loopTimes; i++) {
+        for (int i = 0; i < loopCount; i++) {
             discoveryGuideTestCases.testVersionStrategyGray3(gatewayGroup, gatewayServiceId, gatewayTestUrl);
         }
     }
@@ -176,7 +176,7 @@ public class DiscoveryGuideTest {
     @Test
     public void testVersionWeightStrategyGray() throws Exception {
         if (weightTestcasesEnabled) {
-            for (int i = 0; i < loopTimes; i++) {
+            for (int i = 0; i < loopCount; i++) {
                 discoveryGuideTestCases.testVersionWeightStrategyGray(gatewayGroup, gatewayServiceId, gatewayTestUrl);
             }
         }
@@ -184,20 +184,20 @@ public class DiscoveryGuideTest {
 
     @Test
     public void testRegionStrategyGray1() throws Exception {
-        for (int i = 0; i < loopTimes; i++) {
+        for (int i = 0; i < loopCount; i++) {
             discoveryGuideTestCases.testRegionStrategyGray1(gatewayGroup, gatewayServiceId, gatewayTestUrl);
         }
     }
 
     @Test
     public void testRegionStrategyGray2() throws Exception {
-        for (int i = 0; i < loopTimes; i++) {
+        for (int i = 0; i < loopCount; i++) {
             discoveryGuideTestCases.testRegionStrategyGray2(gatewayGroup, gatewayServiceId, gatewayTestUrl);
         }
     }
 
     public void testRegionStrategyGray3() throws Exception {
-        for (int i = 0; i < loopTimes; i++) {
+        for (int i = 0; i < loopCount; i++) {
             discoveryGuideTestCases.testRegionStrategyGray3(gatewayGroup, gatewayServiceId, gatewayTestUrl);
         }
     }
@@ -205,7 +205,7 @@ public class DiscoveryGuideTest {
     @Test
     public void testRegionWeightStrategyGray() throws Exception {
         if (weightTestcasesEnabled) {
-            for (int i = 0; i < loopTimes; i++) {
+            for (int i = 0; i < loopCount; i++) {
                 discoveryGuideTestCases.testRegionWeightStrategyGray(gatewayGroup, gatewayServiceId, gatewayTestUrl);
             }
         }
@@ -213,147 +213,147 @@ public class DiscoveryGuideTest {
 
     @Test
     public void testStrategyBlueGreen1() throws Exception {
-        for (int i = 0; i < loopTimes; i++) {
+        for (int i = 0; i < loopCount; i++) {
             discoveryGuideTestCases.testStrategyBlueGreen1(gatewayGroup, gatewayServiceId, gatewayTestUrl);
         }
     }
 
     @Test
     public void testStrategyBlueGreen2() throws Exception {
-        for (int i = 0; i < loopTimes; i++) {
+        for (int i = 0; i < loopCount; i++) {
             discoveryGuideTestCases.testStrategyBlueGreen2(gatewayGroup, gatewayServiceId, gatewayTestUrl);
         }
     }
 
     @Test
     public void testStrategyBlueGreen3() throws Exception {
-        for (int i = 0; i < loopTimes; i++) {
+        for (int i = 0; i < loopCount; i++) {
             discoveryGuideTestCases.testStrategyBlueGreen3(gatewayGroup, gatewayServiceId, gatewayTestUrl);
         }
     }
 
     @Test
     public void testStrategyBlueGreenHeader1() throws Exception {
-        for (int i = 0; i < loopTimes; i++) {
+        for (int i = 0; i < loopCount; i++) {
             discoveryGuideTestCases.testStrategyBlueGreenHeader1(gatewayGroup, gatewayServiceId, gatewayTestUrl);
         }
     }
 
     @Test
     public void testStrategyBlueGreenHeader2() throws Exception {
-        for (int i = 0; i < loopTimes; i++) {
+        for (int i = 0; i < loopCount; i++) {
             discoveryGuideTestCases.testStrategyBlueGreenHeader2(gatewayGroup, gatewayServiceId, gatewayTestUrl);
         }
     }
 
     @Test
     public void testStrategyBlueGreenHeader3() throws Exception {
-        for (int i = 0; i < loopTimes; i++) {
+        for (int i = 0; i < loopCount; i++) {
             discoveryGuideTestCases.testStrategyBlueGreenHeader3(gatewayGroup, gatewayServiceId, gatewayTestUrl);
         }
     }
 
     @Test
     public void testStrategyBlueGreenHeaderParameterCookie() throws Exception {
-        for (int i = 0; i < loopTimes; i++) {
+        for (int i = 0; i < loopCount; i++) {
             discoveryGuideTestCases.testStrategyBlueGreenHeaderParameterCookie(gatewayGroup, gatewayServiceId, gatewayTestUrl);
         }
     }
 
     @Test
     public void testStrategyAll1Test1() throws Exception {
-        for (int i = 0; i < loopTimes; i++) {
+        for (int i = 0; i < loopCount; i++) {
             discoveryGuideTestCases.testStrategyAll1(gatewayGroup, gatewayServiceId, gatewayTestUrl, "0");
         }
     }
 
     @Test
     public void testStrategyAll1Test2() throws Exception {
-        for (int i = 0; i < loopTimes; i++) {
+        for (int i = 0; i < loopCount; i++) {
             discoveryGuideTestCases.testStrategyAll1(gatewayGroup, gatewayServiceId, gatewayTestUrl, "1");
         }
     }
 
     @Test
     public void testStrategyAll2Test1() throws Exception {
-        for (int i = 0; i < loopTimes; i++) {
+        for (int i = 0; i < loopCount; i++) {
             discoveryGuideTestCases.testStrategyAll2(gatewayGroup, gatewayServiceId, gatewayTestUrl, "2");
         }
     }
 
     @Test
     public void testStrategyAll2Test2() throws Exception {
-        for (int i = 0; i < loopTimes; i++) {
+        for (int i = 0; i < loopCount; i++) {
             discoveryGuideTestCases.testStrategyAll2(gatewayGroup, gatewayServiceId, gatewayTestUrl, "3");
         }
     }
 
     @Test
     public void testStrategyAll2Test3() throws Exception {
-        for (int i = 0; i < loopTimes; i++) {
+        for (int i = 0; i < loopCount; i++) {
             discoveryGuideTestCases.testStrategyAll2(gatewayGroup, gatewayServiceId, gatewayTestUrl, null);
         }
     }
 
     @Test
     public void testInspectStrategyBlueGreenHeader1() throws Exception {
-        for (int i = 0; i < loopTimes; i++) {
+        for (int i = 0; i < loopCount; i++) {
             discoveryGuideTestCases.testInspectStrategyBlueGreenHeader1(gatewayGroup, gatewayServiceId, gatewayInspectUrl);
         }
     }
 
     @Test
     public void testInspectStrategyBlueGreenHeader2() throws Exception {
-        for (int i = 0; i < loopTimes; i++) {
+        for (int i = 0; i < loopCount; i++) {
             discoveryGuideTestCases.testInspectStrategyBlueGreenHeader2(gatewayGroup, gatewayServiceId, gatewayInspectUrl);
         }
     }
 
     @Test
     public void testInspectStrategyBlueGreenHeader3() throws Exception {
-        for (int i = 0; i < loopTimes; i++) {
+        for (int i = 0; i < loopCount; i++) {
             discoveryGuideTestCases.testInspectStrategyBlueGreenHeader3(gatewayGroup, gatewayServiceId, gatewayInspectUrl);
         }
     }
 
     @Test
     public void testStrategyGray1() throws Exception {
-        for (int i = 0; i < loopTimes; i++) {
+        for (int i = 0; i < loopCount; i++) {
             discoveryGuideTestCases.testStrategyGray1(gatewayGroup, gatewayServiceId, gatewayTestUrl);
         }
     }
 
     @Test
     public void testStrategyGray2() throws Exception {
-        for (int i = 0; i < loopTimes; i++) {
+        for (int i = 0; i < loopCount; i++) {
             discoveryGuideTestCases.testStrategyGray2(gatewayGroup, gatewayServiceId, gatewayTestUrl);
         }
     }
 
     @Test
     public void testStrategyGray3() throws Exception {
-        for (int i = 0; i < loopTimes; i++) {
+        for (int i = 0; i < loopCount; i++) {
             discoveryGuideTestCases.testStrategyGray3(gatewayGroup, gatewayServiceId, gatewayTestUrl);
         }
     }
 
     @Test
     public void testStrategyGray4() throws Exception {
-        for (int i = 0; i < loopTimes; i++) {
+        for (int i = 0; i < loopCount; i++) {
             discoveryGuideTestCases.testStrategyGray4(gatewayGroup, gatewayServiceId, gatewayTestUrl);
         }
     }
 
     @Test
     public void testVersionRuleGray() throws Exception {
-        for (int i = 0; i < loopTimes; i++) {
+        for (int i = 0; i < loopCount; i++) {
             discoveryGuideTestCases.testVersionRuleGray(gatewayGroup, gatewayGroup, gatewayTestUrl);
         }
     }
 
     @Test
     public void testRegionRuleGray() throws Exception {
-        for (int i = 0; i < loopTimes; i++) {
+        for (int i = 0; i < loopCount; i++) {
             discoveryGuideTestCases.testRegionRuleGray(gatewayGroup, gatewayGroup, gatewayTestUrl);
         }
     }
@@ -361,7 +361,7 @@ public class DiscoveryGuideTest {
     @Test
     public void testVersionWeightRuleGray() throws Exception {
         if (weightTestcasesEnabled) {
-            for (int i = 0; i < loopTimes; i++) {
+            for (int i = 0; i < loopCount; i++) {
                 discoveryGuideTestCases.testVersionWeightRuleGray(gatewayGroup, gatewayGroup, gatewayTestUrl);
             }
         }
@@ -370,7 +370,7 @@ public class DiscoveryGuideTest {
     @Test
     public void testRegionWeightRuleGray() throws Exception {
         if (weightTestcasesEnabled) {
-            for (int i = 0; i < loopTimes; i++) {
+            for (int i = 0; i < loopCount; i++) {
                 discoveryGuideTestCases.testRegionWeightRuleGray(gatewayGroup, gatewayGroup, gatewayTestUrl);
             }
         }
@@ -379,7 +379,7 @@ public class DiscoveryGuideTest {
     @Test
     public void testVersionCompositeRuleGray() throws Exception {
         if (weightTestcasesEnabled) {
-            for (int i = 0; i < loopTimes; i++) {
+            for (int i = 0; i < loopCount; i++) {
                 discoveryGuideTestCases.testVersionCompositeRuleGray(gatewayGroup, gatewayGroup, gatewayTestUrl);
             }
         }
@@ -387,49 +387,49 @@ public class DiscoveryGuideTest {
 
     @Test
     public void testBlacklist1() throws Exception {
-        for (int i = 0; i < loopTimes; i++) {
+        for (int i = 0; i < loopCount; i++) {
             discoveryGuideTestCases.testBlacklist1(gatewayGroup, gatewayGroup, gatewayTestUrl);
         }
     }
 
     @Test
     public void testBlacklist2() throws Exception {
-        for (int i = 0; i < loopTimes; i++) {
+        for (int i = 0; i < loopCount; i++) {
             discoveryGuideTestCases.testBlacklist2(gatewayTestUrl);
         }
     }
 
     @Test
     public void testBlacklist3() throws Exception {
-        for (int i = 0; i < loopTimes; i++) {
+        for (int i = 0; i < loopCount; i++) {
             discoveryGuideTestCases.testBlacklist3(gatewayTestUrl);
         }
     }
 
     @Test
     public void testEnv() throws Exception {
-        for (int i = 0; i < loopTimes; i++) {
+        for (int i = 0; i < loopCount; i++) {
             discoveryGuideTestCases.testEnv(gatewayTestUrl);
         }
     }
 
     @Test
     public void testSentinelAuthority1() throws Exception {
-        for (int i = 0; i < loopTimes; i++) {
+        for (int i = 0; i < loopCount; i++) {
             discoveryGuideTestCases.testSentinelAuthority1(gatewayGroup, gatewayTestUrl);
         }
     }
 
     @Test
     public void testSentinelAuthority2() throws Exception {
-        for (int i = 0; i < loopTimes; i++) {
+        for (int i = 0; i < loopCount; i++) {
             discoveryGuideTestCases.testSentinelAuthority2(gatewayGroup, gatewayTestUrl);
         }
     }
 
     @Test
     public void testSentinelAuthority3() throws Exception {
-        for (int i = 0; i < loopTimes; i++) {
+        for (int i = 0; i < loopCount; i++) {
             discoveryGuideTestCases.testSentinelAuthority3(gatewayGroup, gatewayTestUrl);
         }
     }
@@ -442,21 +442,21 @@ public class DiscoveryGuideTest {
 
     /*@Test
     public void testZ1NacosDynamicalMetadataUpdated() throws Exception {
-        for (int i = 0; i < loopTimes; i++) {
+        for (int i = 0; i < loopCount; i++) {
             discoveryGuideTestCases.testNacosDynamicalMetadataUpdated(gatewayGroup, gatewayGroup, gatewayTestUrl);
         }
     }
 
     @Test
     public void testZ2NacosDynamicalMetadataDeleted() throws Exception {
-        for (int i = 0; i < loopTimes; i++) {
+        for (int i = 0; i < loopCount; i++) {
             discoveryGuideTestCases.testNacosDynamicalMetadataDeleted(gatewayGroup, gatewayGroup, gatewayTestUrl);
         }
     }
 
     @Test
     public void testZ3NacosDynamicalMetadataRestored() throws Exception {
-        for (int i = 0; i < loopTimes; i++) {
+        for (int i = 0; i < loopCount; i++) {
             discoveryGuideTestCases.testNacosDynamicalMetadataRestored(gatewayGroup, gatewayGroup, gatewayTestUrl);
         }
     }*/
