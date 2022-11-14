1972418db40c2692399e882b3be81d662d58ee37
HaojunRen <1394997@qq.com>
2022-11-14 15:50:43 +0800

    Bump version to 6.20.0-SNAPSHOT

--- discovery-guide-service/src/main/java/com/nepxion/discovery/guide/service/rest/BRestImpl.java
+++ discovery-guide-service/src/main/java/com/nepxion/discovery/guide/service/rest/BRestImpl.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.nepxion.discovery.common.constant.DiscoveryConstant;
 import com.nepxion.discovery.guide.service.core.CoreImpl;
+import com.nepxion.discovery.plugin.strategy.service.annotation.ServiceMonitorIgnore;
 
 @RestController
 @ConditionalOnProperty(name = DiscoveryConstant.SPRING_APPLICATION_NAME, havingValue = "discovery-guide-service-b")
@@ -25,6 +26,7 @@ public class BRestImpl extends CoreImpl {
     private static final Logger LOG = LoggerFactory.getLogger(BRestImpl.class);
 
     @GetMapping(path = "/rest/{value}")
+    @ServiceMonitorIgnore
     public String rest(@PathVariable(value = "value") String value) {
         value = getPluginInfo(value);
 
